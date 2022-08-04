a6ec33ef58d5a3c188a249a1551dcd6204583f9c
邢志静 <286582988@qq.com>
2022-08-04 18:19:12 +0800

    测试用例配置文件

--- test-case/src/main/java/com/xzj/utils/ConfigFile.java
+++ test-case/src/main/java/com/xzj/utils/ConfigFile.java
@@ -1,19 +1,29 @@
 package com.xzj.utils;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Data
+@Component
+@ConfigurationProperties(prefix = "configfile")
 public class ConfigFile {
-    private static ResourceBundle resourceBundle;
+    private String url;
+
+    private String addUserUrl;
+
+    private String getUserInfoUrl;
+
+    private String updateUserInfoUrl;
 
     /**
      * 获取接口的URL
      *
      * @param interfaceName 请求接口名
      * */
-    public String getUrl(String interfaceName){
-        return getUrl("application", interfaceName);
-    }
+//    public String getUrl(String interfaceName){
+//        return getUrl("application", interfaceName);
+//    }
 
     /**
      * 获取请求的URL
@@ -21,15 +31,17 @@ public class ConfigFile {
      * @param propertiesName 读取的配置文件名
      * @param interfaceName 请求接口名
      * */
-    public static String getUrl(String propertiesName, String interfaceName){
-        resourceBundle = ResourceBundle.getBundle(propertiesName, Locale.CHINA);
-        String host = resourceBundle.getString("test.url");
-//        String interfaceUri = resourceBundle.getString(urlKey);
-        String urlValue = "";
-//        if (apiKey != null && !apiKey.equals("")){
-//            urlValue = interfaceUri + resourceBundle.getString(apiKey);
-//        }
-        return urlValue;
-    }
+//    public static String getUrl(String propertiesName, String interfaceName){
+////        resourceBundle = ResourceBundle.getBundle(propertiesName, Locale.CHINA);
+////        String host = resourceBundle.getString("test.url");
+//
+//        return "";
+//    }
+
+//    public static void main(String[] args) {
+//        System.out.println(ConfigFile.getHostUrl());
+//        ConfigFile configFile = new ConfigFile();
+//        System.out.println(configFile.getAddUserUrl());
+//    }
 
 }
---
+++ test-case/src/test/java/com/xzj/test/TestConfigFile.java
@@ -0,0 +1,14 @@
+package com.xzj.test;
+
+import com.xzj.utils.ConfigFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+public class TestConfigFile {
+    @Autowired
+    ConfigFile configFile;
+    @Test
+    public void testUrl(){
+        System.out.println(configFile.getUrl());
+    }
+}
