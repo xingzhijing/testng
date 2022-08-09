79d25ed006410e6166bdf0059f7c27422e7f5278
邢志静 <286582988@qq.com>
2022-08-09 13:22:34 +0800

    修改配置和增加登录测试用例

--- test-case/src/main/java/com/xzj/utils/UserProperties.java
+++ test-case/src/main/java/com/xzj/utils/UserProperties.java
@@ -1,12 +1,8 @@
 package com.xzj.utils;
 
-import com.xzj.dto.InterfaceName;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
-import static com.xzj.dto.InterfaceName.*;
 
 @Data
 @Component
